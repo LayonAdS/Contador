45517d2ea6349d7162490ebe39d3ff7c321f6173
LayonAdS <locosilia5@gmail.com>
2026-03-12 21:50:56 -0300

    fazendo função atualizar interface e colocando regra de mudança de cor

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.contador;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -24,9 +26,30 @@ public class MainActivity extends AppCompatActivity {
         Button btnDec = findViewById(R.id.btnDecrementar);
 
         // eventos de click para inicrementar
+        btnInc.setOnClickListener(v -> {
+            contador++;
+            atualizarInterface();
+        });
 
-
-
+        btnDec.setOnClickListener(v -> {
+            if (contador > 0) {
+                contador--;
+                atualizarInterface();
+            }
         });
     }
-}
+    // metodo para centralizar e atualizar a ui
+    private void atualizarInterface(){
+        tvContador.setText(String.valueOf(contador));
+
+        // regra de mudança de cor ao atingir limite de 10
+
+        if(contador >= 10){
+            tvContador.setTextColor(Color.RED);
+        }else{
+            tvContador.setTextColor(Color.BLACK);
+        }
+
+    }
+
+    }
